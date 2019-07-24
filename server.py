84948163791409e31ed648da794a0d73965b4926
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-23 17:01:59 -0700

    added /diss and /insult, turned debug off and on

--- server.py
+++ server.py
@@ -18,7 +18,13 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>Hi! This is the home page <br>
+    <a href="/hello">Get a compliment</a><br>
+    <a href="/diss">Get a diss</a>
+    </html>
+    """
 
 
 @app.route("/hello")
@@ -34,7 +40,41 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
+          What's your name? <input type="text" name="person"><br>
+          <label> Choose your compliment: </label>
+            <select name="compliment">
+                <option value="beautiful">beautiful</option>
+                <option value="smart">smart</option>
+                <option value="funny">funny</option>
+                <option value="cheerful">cheerful</option>
+            </select><br>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+@app.route("/diss")
+def say_insult():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/insult">
+          What's your name? <input type="text" name="person"><br>
+          <label> Choose your diss: </label>
+            <select name="diss">
+                <option value="ugly">ugly</option>
+                <option value="stinky">stinky</option>
+                <option value="boring">boring</option>
+                <option value="stupid">stupid</option>
+            </select><br>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,9 +88,8 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
-    y = x
+    # compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
@@ -65,7 +104,28 @@ def greet_person():
     """.format(player, compliment)
 
 
+@app.route("/insult")
+def insult_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    insult = request.args.get("diss")
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>An Insult</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, insult)
+
+
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
